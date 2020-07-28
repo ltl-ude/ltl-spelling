93947fb5ac18dac5e193a6c8b63a646fc2d2479e
Marie Bexte <marie.bexte@stud.uni-due.de>
2020-07-28 10:24:07 +0200

    Fix missing words in hunspell and corresponding bas file for Litkey

--- de.unidue.ltl.spelling/src/main/java/de/unidue/ltl/spelling/utils/GraphemeDictionaryToPhonemeDictionary.java
+++ de.unidue.ltl.spelling/src/main/java/de/unidue/ltl/spelling/utils/GraphemeDictionaryToPhonemeDictionary.java
@@ -19,7 +19,7 @@ public class GraphemeDictionaryToPhonemeDictionary {
 		//deu-DE OR eng-US
 		processDictionary(
 				"/Users/mariebexte/ltl-spelling/de.unidue.ltl.spelling/src/main/resources/dictionaries/hunspell_DE.txt",
-				"/Users/mariebexte/ltl-spelling/de.unidue.ltl.spelling/src/main/resources/dictionaries/hunspell_DE_phoneme.txt",
+				"/Users/mariebexte/ltl-spelling/de.unidue.ltl.spelling/src/main/resources/dictionaries/hunspell_DE_bas_270720.txt",
 				"deu-DE");
 	}
 	
@@ -41,11 +41,13 @@ public class GraphemeDictionaryToPhonemeDictionary {
 		int stepSize = 10000;
 		int numSteps = (int) Math.floor(graphemes.size()/stepSize);
 		for(int i = 0; i<=numSteps; i++) {
+			System.out.println("Processing batch "+(i+1)+"/"+(numSteps+1));
 			if(i == numSteps) {
 				subList = graphemes.subList(i*stepSize, graphemes.size()-1);
 			}
 			else{
-				subList = graphemes.subList(i*stepSize, (i+1)*stepSize-1);
+				subList = graphemes.subList(i*stepSize, (i+1)*stepSize);
+				System.out.println("from "+(i*stepSize)+" to "+ ((i+1)*stepSize-1));
 			}
 			
 			phonemes.addAll(PhonemeUtils.getPhonemes(subList,language));
